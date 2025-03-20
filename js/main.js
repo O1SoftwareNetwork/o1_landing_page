06bca6fe3bc2c79d2df75b075e9ed41c06ac2925
Jim <jamesgabrielpeterson@gmail.com>
2025-03-19 20:06:28 -0400

    add comments and brandon and tiffany code constructor and mouseStart method

--- js/main.js
+++ js/main.js
@@ -3,28 +3,108 @@
  * MOBILE MOBILE MOBILE MOBILE MOBILE MOBILE MOBILE MOBILE MOBILE MOBILE MOBILE MOBILE MOBILE 
  */
 
+// Remove the buttons from the HTML Testimonial Slider
+
 class TestimonialSlider {
     constructor(testimonials) {
         this.testimonials = testimonials;
         this.index = 0;
 
-        this.testimonialName = document.getElementById("testimonial_name");
-        this.testimonialReview = document.getElementById("testimonial_review");
-        this.srcImage = document.getElementById("testimonial_image");
-        this.leftButton = document.getElementById("testimonial__left-button");
-        this.rightButton = document.getElementById("testimonial__right-button");
+        this.testimonialElement = document.querySelector(".testimonial")
 
-        this.leftButton.addEventListener("click", () => this.prevTestimonial());
-        this.rightButton.addEventListener("click", () => this.nextTestimonial());
+        // Mobile
+        this.touchStartX = 0;
+
+        // Desktop
+        this.isDragging = false;
+        this.mouseStartX = 0;
+        
+        // Create the swipe event listeners
+        // Create two event listeners
+        // touchstart (event)
+        // touchStart (Execute the touchStart method and pass in the event)
+        this.testimonialElement.addEventListener("touchstart", (event) => this.touchStart(event));
+        this.testimonialElement.addEventListener("touchend", (event) => this.touchEnd(event));
+
+
+        // Create the swipe simulation event listeners
+        // Create four event listeners
+        // mousedown (event)
+            // Execute the mouseStart and pass in the event object
+        this.testimonialElement.addEventListener("mousedown", (event) => this.mouseStart(event));
+        // mousemove (event)
+            // Execute the mouseMove method and pass in the event object
+        this.testimonialElement.addEventListener("mousemove", (event) => this.mouseMove(event));
+        // mouseup (event)
+            // Execute the mouseEnd method and pass in the event object
+        this.testimonialElement.addEventListener("mouseup", (event) => this.mouseEnd(event));
+        // mouseleave (event)
+            // Execute the mouseLeave method and pass in the event object
+        this.testimonialElement.addEventListener("mouseleave", (event) => this.mouseEnd(event));
     
         this.updateTestimonial();
     }
 
     updateTestimonial () {
         const currentTestimonial = this.testimonials[this.index];
+        // Instead of setting the testimonial_name, testimonial, and testimonial_image individually
+        // we'll set the innerHTML of the testimonial element using a template literal and injecting
+        // JavaScript variables from the testimonial object
+        // (Remember to include the classes and ids)
         this.testimonialName.textContent = currentTestimonial.testimonial_name;
         this.testimonialReview.textContent = currentTestimonial.testimonial;
         this.srcImage.src = currentTestimonial.testimonial_image;
+    }
+
+    // Pass the event into the method
+    touchStart() {
+       // Set the variable using the event object 
+    }
+
+    // Pass the event into the method
+    touchEnd() {
+        // Create a variable to determine where the finger was positioned when the touch ended
+
+        // Create a variable to determine the difference between the starting and ending position
+
+        // Create a comnditional statement
+        // If the difference is greater than 50
+            // Next Testimonial
+        // If the difference is less than -50
+            // Previous Testimonial
+    }
+
+    // Pass the event into the method
+    mouseStart(tc) {
+        this.isDragging = true;
+        // Set the starting position
+        this.mouseStartX = tc.clientX;
+    }
+
+    // Pass the event into the method
+    mouseMove() {
+        // Log the position of the mouse to the console
+    }
+
+    // Pass the event into the method
+    mouseEnd() {
+        // Check to see if the user is moving their mouse with dragging variable
+        // If it's false just return
+
+        // Create a variable to to store the position of the mouse when the user stopped clicking
+
+        // Create a variable finding the differenc between the start and end position
+
+        // If the difference is greater than 50 with Math absolute
+        // Set the dragging to false
+        // If the difference is greater than 0
+            // Next Testimonial
+        // If not
+            // Previous Testimonial
+    }
+
+    mouseLeave() {
+        this.isDragging = false;
     }
 
     nextTestimonial() {
@@ -61,11 +141,9 @@ class TestimonialCarousel {
             next: null
         }
 
-        this.leftButton = document.getElementById("card-container__left-button");
-        this.rightButton = document.getElementById("card-container__right-button");
-        
-        this.leftButton.addEventListener("click", () => this.navigate("prev"));
-        this.rightButton.addEventListener("click", () => this.navigate("next"));
+        this.previousCard = document.getElementById("previous-card");
+        this.currentCard = document.getElementById("current-card");
+        this.nextCard = document.getElementById("next-card");
 
         this.render();
     }
@@ -78,13 +156,21 @@ class TestimonialCarousel {
 
     render() {
         this.updateIndexes(this.indexes.current);
-        this.renderCard(this.reviews[this.indexes.previous], "previous-card");
-        this.renderCard(this.reviews[this.indexes.current], "current-card");
-        this.renderCard(this.reviews[this.indexes.next], "next-card");
+        this.renderCard(this.reviews[this.indexes.previous], this.previousCard);
+        this.renderCard(this.reviews[this.indexes.current], this.currentCard);
+        this.renderCard(this.reviews[this.indexes.next], this.nextCard);
+
+        this.previousCard.replaceWith(this.previousCard.cloneNode(true));
+        this.nextCard.replaceWith(this.nextCard.cloneNode(true));
+
+        this.previousCard = document.getElementById("previous-card");
+        this.nextCard = document.getElementById("next-card");
+
+        this.previousCard.addEventListener("click", () => this.navigate("prev"));
+        this.nextCard.addEventListener("click", () => this.navigate("next"));
     }
 
-    renderCard(review, elementId) {
-        const element = document.getElementById(elementId);
+    renderCard(review, element) {
         element.innerHTML = "";
     
         const header = document.createElement("h2");
