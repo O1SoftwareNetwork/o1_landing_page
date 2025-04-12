86d18d11f4cea8c803e53a4dc8091f010f3f315b
Jim <jamesgabrielpeterson@gmail.com>
2025-04-12 16:14:59 -0400

    workshop 4/12/2025 - Add tech stack icons, fix testimonial slider, mobile proejcts cards

--- js/main.js
+++ js/main.js
@@ -31,13 +31,12 @@ class TestimonialSlider {
         const currentTestimonial = this.testimonials[this.index];
        
         this.testimonialElement.innerHTML = `
-            <div class="testimonials__person">
-                <img id="testimonial_image" class="testimonials__person-image" src="${currentTestimonial.testimonial_image}" />
+            <div class="testimonials__component">
                 <div class="testimonials__person-name">
                     <span id="testimonial_name">${currentTestimonial.testimonial_name}</span>
                 </div>
             </div>
-            <p id="testimonial_review" class="testimonials__review">${currentTestimonial.review}"</p>
+            <p id="testimonial_review" class="testimonials__review">${currentTestimonial.testimonial}</p>
         `;
     }
 
@@ -100,10 +99,10 @@ class TestimonialSlider {
 }
 
 const testimonials = [
-    { testimonial_name: "John Doe", testimonial: "Great experience! Highly recommend!", testimonial_image: "Images/profiles/profile-1.png"},
-    { testimonial_name: "Tiffany ", testimonial: "Lovely!", testimonial_image: "Images/profiles/profile-2.png"},
-    { testimonial_name: "Brandon", testimonial: "Can't wait to come back", testimonial_image: "Images/profiles/profile-3.jpg"}, 
-    { testimonial_name: "T", testimonial: "I really enjoyed the workshop!", testimonial_image: "Images/profiles/profile-4.webp"},
+    { testimonial_name: "John Doe", testimonial: "Great experience! Highly recommend! Lorem ipsum odor amet, consectetuer adipiscing elit. Taciti felis netus mattis nascetur;  tempus suspendisse. Taciti felis netus mattis nascetur;  tempus suspendisse. Taciti felis netus mattis nascetur;  tempus suspendisse."},
+    { testimonial_name: "Tiffany ", testimonial: "Lovely! Lorem ipsum odor amet, consectetuer adipiscing elit. Taciti felis netus mattis nascetur;  tempus suspendisse. Taciti felis netus mattis nascetur;  tempus suspendisse. Taciti felis netus mattis nascetur;  tempus suspendisse."},
+    { testimonial_name: "Brandon", testimonial: "Can't wait to come back! Lorem ipsum odor amet, consectetuer adipiscing elit. Taciti felis netus mattis nascetur;  tempus suspendisse. Taciti felis netus mattis nascetur;  tempus suspendisse. Taciti felis netus mattis nascetur;  tempus suspendisse."}, 
+    { testimonial_name: "T", testimonial: "I really enjoyed the workshop! Lorem ipsum odor amet, consectetuer adipiscing elit. Taciti felis netus mattis nascetur;  tempus suspendisse. Taciti felis netus mattis nascetur;  tempus suspendisse. Taciti felis netus mattis nascetur;  tempus suspendisse."},
 ];
 
 const testimonialSlider = new TestimonialSlider(testimonials);
